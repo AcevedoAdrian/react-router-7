b705da303edd7a4be0bcca766300a68ffe53472c
AcevedoAdrian <acevedohugoadrian@gmail.com>
2025-04-29 07:55:54 -0300

    Agrega nuevas dependencias de Radix UI y React Router, actualiza el componente Button para mejorar la gestión de props y refactoriza el archivo App.tsx para utilizar AppRouter. También se realizan ajustes en el archivo utils.ts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from "./components/ui/button";
+import { AppRouter } from "./AppRouter";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hola Mundo</h1>
-      <Button variant="outline">Click me</Button>
+      <AppRouter />
     </>
   );
 }
---
+++ src/AppRouter.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { AuthLayout } from "./auth/layout/AuthLayout";
+import { RegisterPage } from "./auth/pages/RegisterPage";
+import { LoginPage } from "./auth/pages/LoginPage";
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route index element={<LoginPage />} />
+          <Route path="/auth/register" element={<RegisterPage />} />
+        </Route>
+        <Route path="/" element={<Navigate to="/auth" />}></Route>
+        <Route path="*" element={<Navigate to="/auth" />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
